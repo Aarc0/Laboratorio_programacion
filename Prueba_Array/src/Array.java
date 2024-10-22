dedcf9b56117e01782761e3801cc5ec485f95f5a
andre <andre@DESKTOP-2D3OFPM>
2024-10-22 22:00:21 +0200

    Dos matrices

--- Prueba_Array/src/Array.java
+++ Prueba_Array/src/Array.java
@@ -4,41 +4,15 @@ public class Array
 {
 	public static void main(String[] args) 
 	{
-		Scanner n = new Scanner(System.in);
-		
-		System.out.println("Filas: ");
-		int F = n.nextInt();
-		System.out.println("Columas: ");
-		int C = n.nextInt();
-		int[][] numeros = new int[F][C];
-		
-		for(int i = 0; i < F ; i++)
-		{
-			for(int j = 0; j < numeros[i].length ; j++)
-			{
-				numeros[i][j] = n.nextInt();
-			}
-		}
-		
-	}
-}
-
-
-/*import java.util.*;
-
-public class Array 
-{
-	public static void main(String[] args) 
-	{
 		
 		Scanner n = new Scanner(System.in);
-		System.out.println("Filas: ");
+		System.out.print("Inserte el numero de filas para la matriz 1: ");
 		int F = n.nextInt();
-		System.out.println("Columas: ");
+		System.out.print("Inserte el numero de columnas para la matriz 1: ");
 		int C = n.nextInt();
-		int[][] numeros = new int[F][C];
+		int[][] matriz1	=new int [F][C];
 		//Se le ingresa a la arrey un nº de filas y de columnas
-		
+		     
 		for(int i = 0; i < F ; i++)
 		//si i es menor que el numero de filas entra
 		{
@@ -46,10 +20,48 @@ public class Array
 			for(int j = 0; j < C ; j++)
 			//si j es menor que el numero de indices que hay en la fila del valor que tenga j, entra 
 			{
-				numeros[i][j] = n.nextInt();
+				System.out.print("Inserta el valor para la fila " + (i+1) + ", columna " + (j+1)+ ": ");
+				matriz1[i][j]=n.nextInt();
 			}
-			
+		}
+		for(int i = 0; i < F ; i++)
+		//si i es menor que el numero de filas entra
+		{
+				
+			for(int j = 0; j < C ; j++)
+			//si j es menor que el numero de indices que hay en la fila del valor que tenga j, entra 
+			{
+				System.out.print("La fila " + (i+1) + ", columna " + (j+1)+ " de la matriz 1, tiene almacenado el valor: " + matriz1[i][j] + "\n\n");
+			}
 		}
 		
+		System.out.print("Inserte el numero de filas para la matriz 2: ");
+		int F2 = n.nextInt();
+		System.out.print("Inserte el numero de columnas para la matriz 2: ");
+		int C2 = n.nextInt();
+		int[][] matriz2	=new int [F2][C2];
+		//Se le ingresa a la arrey un nº de filas y de columnas
+		     
+		for(int i = 0; i < F2 ; i++)
+		//si i es menor que el numero de filas entra
+		{
+			
+			for(int j = 0; j < C2 ; j++)
+			//si j es menor que el numero de indices que hay en la fila del valor que tenga j, entra 
+			{
+				System.out.print("Incerta el valor para la fila " + (i+1) + ", columna " + (j+1)+ ": ");
+				matriz2[i][j] = n.nextInt();
+			}
+		}
+		for(int i = 0; i < F2 ; i++)
+		//si i es menor que el numero de filas entra
+		{
+				
+			for(int j = 0; j < C2 ; j++)
+			//si j es menor que el numero de indices que hay en la fila del valor que tenga j, entra 
+			{
+				System.out.print("La fila " + (i+1) + ", columna " + (j+1)+ " de la matriz 1, tiene almacenado el valor: " + matriz2[i][j] + "\n\n");
+			}
+		}
 	}
-}*/
+}
